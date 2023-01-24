4ad3c725cd1abd08377a6c55040d7ee8ed0cdac8
moraisvitoria <112016943+moraisvitoria@users.noreply.github.com>
2023-01-24 16:41:51 -0300

    Add files via upload

---
+++ adm.php
@@ -0,0 +1,12 @@
+<?php
+  require_once 'head.php';
+
+    //session_start();
+    ob_start();
+  
+
+?>  
+
+<a href="frmaluno.php"><button type="submit">Cadastro do usuario</button></a>
+
+<a href="relalunos.php"><button type="submit">Relatório de usuario</button></a>
---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer class="">
+<div class="container-fluid">
+      <div class="row text-center bg-light">
+        <div class="col-md-3">
+          <h4>Institucional</h4>
+          <p><a href="#">Home</a></p>
+          <p><a href="#">Floreça</a></p>
+          <p><a href="#">agradecemos a visita</a></p>
+          
+        </div>
+
+        <div class="col-md-3">
+          <h4>Atividades</h4>
+          <ul class="navbar-nav mr-auto">
+            <li class="navbar-item"><a class="nav-link" href="#">cabelo</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">maquiagem</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">Dicas  de um alimenteção boa</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">atividade fisica</a></li>
+          </ul>
+        </div>
+
+    
+        
+        <div class="col-md-3 mapa">
+          <h4>Localização</h4>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.356219020248!2d-43.56103518503453!3d-22.900228385015083!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be15839e68c4f%3A0x588a284ae162bc38!2sSenac%20Campo%20Grande!5e0!3m2!1spt-PT!2sbr!4v1669141477166!5m2!1spt-PT!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+
+      <div class="row text-center">
+        <div class="col-md-12 sociais">
+          <a href="#"><img src="imagens/whatsapp.png"></a>
+          <a href="#"><img src="imagens/face.png"></a>
+          <a href="#"><img src="imagens/insta.png"></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+     <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
